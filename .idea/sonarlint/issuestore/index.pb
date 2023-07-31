7ea67bc1a8e9d3d3592ba79a3133ea8e70e07ac4
Luan Silva de Sena Advincula <luansena@ciandt.com>
2023-07-30 21:11:33 -0300

    fixed: sincronismo na leitura da entrada

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+V
+&src/main/java/org/luansena/example.asm,c/e/cefcc993f7b7a8f8679f27819fd75d53b001b6a1
+_
+/src/main/java/org/luansena/antlr/MIPSLexer.java,d/b/dbd8339677f37f06dd612257e328b8e04869069d
+a
+1src/main/java/org/luansena/antlr/MIPSVisitor.java,b/3/b32a2add38b0724266d7076a8e4e76d3b45cfd8c
+\
+,src/main/java/org/luansena/antlr/MIPS.interp,a/f/affcfe33b7265ee9a65b039730708286aa7e69e9
--- src/main/java/org/luansena/App.java
+++ src/main/java/org/luansena/App.java
@@ -4,69 +4,48 @@ import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.luansena.antlr.MIPSLexer;
 import org.luansena.antlr.MIPSParser;
+import org.luansena.hardware.Memory;
 import org.luansena.parser.MIPSVisitor;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
 import java.util.concurrent.Semaphore;
 
 public class App {
 
-    private static Semaphore semaphore = new Semaphore(0);
+    private static final Semaphore semaphore = new Semaphore(0);
 
     public static void main( String[] args ) {
         String filePath = "/home/luansena/example.asm";
 
-        Thread fileReaderThread = new Thread(() -> {
-            try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-                String line;
-                while ((line = br.readLine()) != null) {
-                    System.out.println("Linha do arquivo: " + line);
-                    // Aqui você pode chamar o parser para processar a linha do arquivo
-                    // ou fazer qualquer outra ação necessária.
-                    MIPSLexer lexer = new MIPSLexer(CharStreams.fromString(line));
-                    CommonTokenStream tokens = new CommonTokenStream(lexer);
-                    MIPSParser parser = new MIPSParser(tokens);
-                    MIPSParser.ProgramContext programContext = parser.program();
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            Scanner scanner = new Scanner(System.in);
+            String line;
 
-                    MIPSVisitor visitor = new MIPSVisitor();
+            while((line = br.readLine()) != null) {
+                System.out.println("Linha do arquivo: " + line);
+                System.out.print("Pressione Enter para continuar...");
+                scanner.nextLine();
 
-                    visitor.visit(programContext);
+                MIPSLexer lexer = new MIPSLexer(CharStreams.fromString(line));
+                CommonTokenStream tokens = new CommonTokenStream(lexer);
+                MIPSParser parser = new MIPSParser(tokens);
+                MIPSParser.ProgramContext programContext = parser.program();
 
-                    try {
-                        semaphore.acquire(); // Pausa a leitura do arquivo até que a tecla Enter seja pressionada
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+                MIPSVisitor visitor = new MIPSVisitor();
+
+                visitor.visit(programContext);
+
             }
-        });
-
-        // Thread para ler a entrada do teclado
-        Thread keyboardInputThread = new Thread(() -> {
-            System.out.println("Pressione Enter para continuar...");
-            try {
-                System.in.read(); // Aguarda a tecla Enter ser pressionada
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            semaphore.release(); // Libera a leitura do arquivo quando a tecla Enter é pressionada
-        });
-
-        fileReaderThread.start();
-        keyboardInputThread.start();
-
-        try {
-            // Aguarda o término da leitura do arquivo
-            fileReaderThread.join();
-            // A thread de leitura do teclado não precisa ser aguardada, pois ela termina após pressionar Enter.
-        } catch (InterruptedException e) {
+        } catch(IOException e) {
             e.printStackTrace();
         }
 
         System.out.println("Fim do programa.");
+        System.out.println(Arrays.toString(Memory.values()));
     }
 }
+
